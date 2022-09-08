8a5d7e3be71a7ecfb4a7e5899e399e0bd105dfe7
Matute12 <mateohofb@gmail.com>
2022-09-08 18:10:32 -0300

    Series en Home

--- movietech/src/components/Series/Series.js
+++ movietech/src/components/Series/Series.js
@@ -1,22 +1,22 @@
 import React,{Component} from 'react'
-import PeliculasCard from '../PeliculasCard/PeliculasCard';
+import SeriesCard from '../SeriesCard/SeriesCard';
 
 
-class Peliculas extends Component {
+class Series extends Component {
     constructor(props){
         super(props)
         this.state={
-            data: [],
-            verMas: "hide"
+            data: []
             
         }
     }
+   
 
     componentDidMount(){
-        fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c')
+        fetch('https://api.themoviedb.org/3/tv/popular?api_key=7a176cc95147be6e695be2faf0e8ff9c')
         .then(resp => resp.json())
         .then(data => this.setState({
-            data: data.results
+            data: data.results.slice(0,12)
         }))
         .catch(err => console.log(err)) 
     }
@@ -26,28 +26,26 @@ class Peliculas extends Component {
     return (
         <>
         <div className="palabra">
-            <h3>PELÍCULAS POPULARES</h3>
+            <h3>SERIES POPULARES</h3>
         </div>
         <section className="card-container">
             {
                 this.state.data.length > 0 ?
                     this.state.data.map((key, idx) => 
-                    <PeliculasCard 
+                    <SeriesCard 
                     key={key + idx} 
-                    name={key.title} 
+                    name={key.name} 
                     image={key.poster_path}
                     descripcion={key.overview}
                     id = {key.id}
-                    agregar = {(id) => this.agregarFavoritos(id)}
-                    
                     />):
                 <h1>Cargando..</h1>
             }
-      </section>
+        </section>
         </>
     )
   }
 }
 
 
-export default Peliculas
+export default Series
--- movietech/src/screen/Home/index.js
+++ movietech/src/screen/Home/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Peliculas from "../../components/Peliculas/Peliculas.js";
+import Series from "../../components/Series/Series";
+
 
 function Home(){
 
     return(
-        <div>
-            <h1>Estrenos</h1>
+        <>
+        <div>     
+            <h1>Estrenos de Peliculas</h1>
             <Peliculas/>
         </div>
+        <div>
+            <h1>Estrenos de Series</h1> 
+            <Series/>
+        </div>
+        </>
     )
+    
 }
 
 export default Home;
