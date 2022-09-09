2831b9eff1955f5e62d0ae43d22ed1ccce6145d0
Brunoo-G <bruno_nicolas88@hotmail.com>
2022-09-08 22:36:37 -0300

    NotFound

--- movietech/src/App.js
+++ movietech/src/App.js
@@ -6,19 +6,21 @@ import Home from "./screen/Home/index"
 import Favoritos from "./screen/Favoritos/index"
 import Series from "./screen/Series/index"
 import Peliculas from "./screen/Peliculas/index"
+import NotFound from "./screen/NotFound/index"
 import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <Header />
+
       <Switch>
-        <Route exact path = "/" component={Home}/>
-        <Route path = "/favoritos" component={Favoritos}/>
-        <Route path = "/Series" component={Series}/>
-        <Route path = "/Peliculas" component={Peliculas}/>
+        <Route exact path = "/" component={Home} />
+        <Route path = "/favoritos" component={Favoritos} />
+        <Route path = "/Series" component={Series} />
+        <Route path = "/Peliculas" component={Peliculas} />
+        <Route component={NotFound} />
       </Switch>  
-      
       
       <Footer />
     </div>
---
+++ movietech/src/screen/NotFound/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+
+function NotFound() {
+    return(
+        <div className="error">
+            <img src="/img/error.png"/>
+        </div>
+    )
+}
+
+export default NotFound;
